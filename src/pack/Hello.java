741003ff3f6671a9d0abee37e7f7ece0cf78b29d
CoderOfArtMagic95 <Sassyash74@yahoo.com>
2021-02-10 01:48:54 -0500

    Adjusted Hello.java to use the Scanner object/class and add more code to make a little Narrator scene between user and computer

--- src/pack/Hello.java
+++ src/pack/Hello.java
@@ -1,16 +1,27 @@
 package pack;
+import java.util.Scanner;
 
 public class Hello {
- public void Message(String name) {
-	 System.out.println(name + ", say hello to the world!");
+ public void Message() {
+	 Scanner scan = new Scanner(System.in);
+	 System.out.println("What is your name? ");
+	 String name = scan.nextLine();
+	 scan.close();
+	 
+	 System.out.printf("Narrator: " + "Hello %s!\n", name);
+	 
+	 System.out.println("Narrator: "+ name + ", say hello to the world!");
 	 System.out.println(name + ": Hello World!");
-	 System.out.println(name + ", who's an otaku coder!");
+	 System.out.println("Narrator: " + name + ", who's an otaku coder!");
 	 System.out.println(name + ": You are!");
 	//System.out.print(); prints on same line as bottom as
 	 //long as its between to println() or print() statements
      System.out.print(name + ": Nothing like enjoying delicious black coffee!");
-     System.out.print(" Earth's caffinated bean juice!");
+     System.out.print("\n  Earth's caffinated bean juice!");
      System.out.println(" The elixir to coding Valhala!");
 	
+     System.out.printf("Narrator: " + "Goodbye %s!\n", name);
+     System.out.printf(name + " : Goodbye %s!\n",name);
+     System.out.printf(name + ": Pfftt!...No wait.. Goodbye Narrator hehe..");
  }
 }
--- src/pack/Message.java
+++ src/pack/Message.java
@@ -4,7 +4,7 @@ public class Message {
 
 	public static void main(String[] args) {
 		Hello message = new Hello();
-		message.Message("Ashley");
+		message.Message();
 
 	}
 
