5cb27ec17ba4d38647d489adfa4f24a8f4ec09df
CoderOfArtMagic95 <Sassyash74@yahoo.com>
2021-02-10 00:43:49 -0500

    Added onto HelloWorld

--- src/pack/Hello.java
+++ src/pack/Hello.java
@@ -4,9 +4,13 @@ public class Hello {
  public void Message(String name) {
 	 System.out.println(name + ", say hello to the world!");
 	 System.out.println(name + ": Hello World!");
-	 
 	 System.out.println(name + ", who's an otaku coder!");
 	 System.out.println(name + ": You are!");
-	 
+	//System.out.print(); prints on same line as bottom as
+	 //long as its between to println() or print() statements
+     System.out.print(name + ": Nothing like enjoying delicious black coffee!");
+     System.out.print(" Earth's caffinated bean juice!");
+     System.out.println(" The elixir to coding Valhala!");
+	
  }
 }
