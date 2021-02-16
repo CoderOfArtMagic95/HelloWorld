b4d63a84263cdc7c2b8e506593b6b4dd4c42c234
CoderOfArtMagic95 <Sassyash74@yahoo.com>
2021-02-16 16:28:13 -0500

    Adjust previous classes and added Lab class for Variables Lab project

--- src/pack/Hello.java
+++ src/pack/Hello.java
@@ -2,11 +2,40 @@ package pack;
 import java.util.Scanner;
 
 public class Hello {
- public void Message() {
+	//undeclared instances/fields
+	int methodCount;
+	String className;
+	String userName;
+	
+	//constructor method
+	public Hello(int count, String NameOfClass, String yourName){
+		methodCount = count++;
+		className = NameOfClass;
+		userName = yourName;
+	}	
+    public Hello() {
+		// TODO Auto-generated constructor stub
+    	methodCount++;
+	}
+    public int methodCounter(boolean isRunning) {
+    	isRunning = true;
+    	if(isRunning == true) {
+    		methodCount++;
+    	}
+    	return methodCount;
+    }
+public void Message() {
+	 
+	 //made before Java Data Types and Variables LAB
+	 methodCounter(true);
 	 Scanner scan = new Scanner(System.in);
 	 System.out.println("What is your name? ");
 	 String name = scan.nextLine();
 	 scan.close();
+	
+	 //aren't null because it is set in method and resides in the method
+	 System.out.println("Value of name: " + name); 
+	 System.out.println(); //used to create an empty line in text 
 	 
 	 System.out.printf("Narrator: " + "Hello %s!\n", name);
 	 
@@ -17,11 +46,13 @@ public class Hello {
 	//System.out.print(); prints on same line as bottom as
 	 //long as its between to println() or print() statements
      System.out.print(name + ": Nothing like enjoying delicious black coffee!");
-     System.out.print("\n  Earth's caffinated bean juice!");
+     System.out.println(); //used to create an empty line in text 
+     System.out.print(name + "Earth's caffinated bean juice!");
      System.out.println(" The elixir to coding Valhala!");
 	
      System.out.printf("Narrator: " + "Goodbye %s!\n", name);
      System.out.printf(name + " : Goodbye %s!\n",name);
      System.out.printf(name + ": Pfftt!...No wait.. Goodbye Narrator hehe..");
+   //the above code has been made before Java Data Types and Variables LAB
  }
 }
--- src/pack/Message.java
+++ src/pack/Message.java
@@ -3,9 +3,32 @@ package pack;
 public class Message {
 
 	public static void main(String[] args) {
-		Hello message = new Hello();
+		//Updated main method
+		//creates a Hello Object with a default constructor
+		Hello talk = new Hello();
+		/* integer that's set to the methodCount in the Hello Object talk
+		 * and is also used in the message Object's constructor below */
+		 int mainInt = talk.methodCount;
+		/* string literals that will be 
+		 * use in the message Object's constructor with parameters or arguments below*/
+		String name = talk.userName;
+		String className1 = talk.className;
+		
+		//Object hello
+		Hello message = new Hello(mainInt, className1 ,name);
+		
+		//undeclared boolean variable 
+		boolean count;
+		//sets count in method call and sets it to true
+		message.methodCounter(count = true);
+		
+		//was here before update
 		message.Message();
-
+		//Prints out the values of the constructor with arguments
+		System.out.println("Value of className1: "+ name); //are null because it is no longer in use once the method has been called
+		System.out.println("Value of name: " + className1); //are null because it is no longer in use once the method has been called
+		//prints the methodCounter value
+	    System.out.println("Total method calls: " + message.methodCounter(count)); //prints 4 because the constructors are also methods
 	}
 
 }
